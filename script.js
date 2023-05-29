0d92aba627780188045cff16595660dd8080b2ba
JOHNATAS_21332 <JOHNATAS_21332@etec.edu>
2023-05-29 07:57:25 -0300

    TCC dos Malucão

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+   document.getElementById("menuOculto").style.width="0";
+   document.getElementById("principal").style.width="0";
+}
+function fecharNav(){
+   document.getElementById("menuOculto").style.width="250px";
+   document.getElementById("principal").style.width="250px";
+}
+/*Carrosel*/
